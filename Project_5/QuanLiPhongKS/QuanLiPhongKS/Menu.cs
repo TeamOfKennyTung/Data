053297af5d2be9117c69bfeff82006ccf72b13dd
luanmu1994 <luanjr1994@gmail.com>
2015-11-12 22:32:40 +0700

    Hoan thien lan 2 Project_5

--- Project_5/QuanLiPhongKS/QuanLiPhongKS/Menu.cs
+++ Project_5/QuanLiPhongKS/QuanLiPhongKS/Menu.cs
@@ -10,11 +10,39 @@ using System.Windows.Forms;
 
 namespace QLPhongKS
 {
-    public partial class Menu : Form
+    public partial class frmMenu : Form
     {
-        public Menu()
+        public frmMenu()
         {
             InitializeComponent();
+        }
+
+        private void phòngToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmPhong phong = new frmPhong();
+            phong.Show();
+            Hide();
+        }
+
+        private void quảnLíThiếtBịToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmTBThietBi tb = new frmTBThietBi();
+            tb.Show();
+            Hide();
+        }
+
+        private void kháchHàngToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmKhachHang kh = new frmKhachHang();
+            kh.Show();
+            Hide();
+        }
+
+        private void thuêPhòngToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmThuePhong thue = new frmThuePhong();
+            thue.Show();
+            Hide();
         }
     }
 }
--- Project_5/QuanLiPhongKS/QuanLiPhongKS/Program.cs
+++ Project_5/QuanLiPhongKS/QuanLiPhongKS/Program.cs
@@ -16,7 +16,7 @@ namespace QLPhongKS
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FormKhachHang());
+            Application.Run(new frmMenu());
         }
     }
 }
