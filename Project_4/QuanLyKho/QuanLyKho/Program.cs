f041374b56f81077414bfbc15e2d7920fffd25f5
Ubuntumta <Ubuntumta@gmail.com>
2015-11-26 08:15:08 +0700

    Còn phần tìm kiếm chưa làm :)))

--- Project_4/QuanLyKho/QuanLyKho/Program.cs
+++ Project_4/QuanLyKho/QuanLyKho/Program.cs
@@ -16,7 +16,7 @@ namespace QuanLyKho
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmNhapHang());
+            Application.Run(new frmDangNhap());
         }
     }
 }
